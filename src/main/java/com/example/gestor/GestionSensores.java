f7945825eb235ffc0e9fa8ea845867a6368766ff
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-12-03 10:52:17 +0100

    feat/metodos principales del gestor

--- src/main/java/com/example/gestor/GestionSensores.java
+++ src/main/java/com/example/gestor/GestionSensores.java
@@ -5,8 +5,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class GestionSensores {
 
+    public void crearSensor(){}
+    public void eliminarSensor(){}
+    public void agregarSensor(){}
+    public void recibirAlertas(){}
     //limitar el numero de sensores por tipo
-    public void numeroSensores(){
+    public void numeroSensores(){}
+    //actualizar los valores si se resuelven los problemas
+    public void actualizarValores(){}
 
-    }
+
 }
