751c727412c01a7d223b254431567ee74c9d6350
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-12-11 19:11:45 +0100

    fix/avance

--- src/main/java/com/example/sensorElectricidad/SensorElectricidadService.java
+++ src/main/java/com/example/sensorElectricidad/SensorElectricidadService.java
@@ -23,11 +23,11 @@ public class SensorElectricidadService {
         List<SensorElectricidad> sensorElectricidad = sensorElectricidadRepository.findAll();
         for (SensorElectricidad sensor : sensorElectricidad) {
             if (sensor.estaEncendido()) {
-                sensor.setPerdidas(simularPerdidas());
-               logger.info("Perdidas detectadas con una estimacion de: " + sensor.getPerdidas()
+                long perdidas = simularPerdidas();
+                sensor.setPerdidas(perdidas);
+                logger.info("Perdidas detectadas con una estimacion de: " + sensor.getPerdidas()
                         +"Kw/h"+ " en el sensor Nº: " + sensor.getId());
-                sensorElectricidadRepository.save(sensor); //esto guarda las perdidas, hay que crear
-                // una tabla relacional para el valor total por id de sensor
+                sensorElectricidadRepository.save(sensor); //esto guarda las perdidas, se debe recuperar en el repositorio de Electricidad para el SmartGrid
             } else {
                 logger.warning("El sensor Nº: " + sensor.getId() + " esta apagado");
             }
@@ -35,14 +35,11 @@ public class SensorElectricidadService {
     }
 
     public long simularPerdidas(){
-        int valorRnd = (int) (Math.random() * 1000);
-        return valorRnd; //perdidas
+      return (long) (Math.random()*1000);
     }
     //el valor tiene que ser leido en sus repositorios para que puedan utilizarlo
 
-    public void avisoElectricidad(){
-        randomizacionEventos.manejarCiclo("luz", ); //ver como poner el SensorElectricidad para incluirlo como parametro
-
+    public void avisoElectricidad(SensorElectricidad sensorElectricidad) {
+        randomizacionEventos.manejarCiclo("luz",sensorElectricidad.getReloj(), this::perdidas);
     }
-
 }
